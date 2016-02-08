0212b0a957b05e2df96fd85994f23a1b7dcd096b
wolf <wolf@debian-wolf>
2016-02-08 11:37:04 +0800

    Simplified makefiles.

---
+++ config.mk
@@ -0,0 +1,35 @@
+
+# If your compiler cannot find the boost's location independently, please specify them explicitly like this(please uncomment them first):
+#boost_include_dir = /usr/local/include/
+#boost_lib_dir = /usr/local/lib/
+
+cflag = -Wall -fexceptions -std=c++11
+ifeq (${MAKECMDGOALS}, debug)
+	cflag += -g -DDEBUG
+	dir = debug
+else
+	cflag += -O2 -DNDEBUG
+	lflag = -s
+	dir = release
+endif
+cflag += -pthread ${ext_cflag} ${boost_include_dir}
+lflag += -pthread ${boost_lib_dir} -lboost_system -lboost_thread ${ext_libs}
+
+target = ${dir}/${module}
+sources = ${shell ls *.cpp}
+objects = ${patsubst %.cpp,${dir}/%.o,${sources}}
+deps = ${patsubst %.o,%.d,${objects}}
+${shell mkdir -p ${dir}}
+
+release debug : ${target}
+-include ${deps}
+${target} : ${objects}
+	${CXX} -o ${target} ${objects} ${lflag}
+${objects} : ${dir}/%.o : %.cpp
+	${CXX} ${cflag} -E -MMD -MT '${subst .cpp,.o,${dir}/$<}' -MF ${subst .cpp,.d,${dir}/$<} $< 1>/dev/null
+	${CXX} ${cflag} -c $< -o $@
+
+.PHONY : clean
+clean:
+	-rm -rf debug release
+
