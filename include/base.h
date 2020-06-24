a4377dd2b68caeebb25c846d915006335246b3f3
youngwolf <li.10.yang@nokia.com>
2020-06-24 17:46:00 +0800

    Fix possibility of memory leaks for auto_buffer.

--- include/base.h
+++ include/base.h
@@ -132,7 +132,7 @@ public:
 	~auto_buffer() {clear();}
 
 	buffer_type raw_buffer() const {return buffer;}
-	void raw_buffer(buffer_type _buffer) {buffer = _buffer;}
+	void raw_buffer(buffer_type _buffer) {clear(); buffer = _buffer;}
 
 	//the following five functions are needed by st_asio_wrapper
 	bool empty() const {return NULL == buffer || buffer->empty();}
