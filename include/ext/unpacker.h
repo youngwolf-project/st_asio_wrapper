e28c9f803d3a828b971112b338963d5c5396f04d
youngwolf <mail2tao@163.com>
2020-12-31 09:30:06 +0800

    Optimize unpackers.

--- include/ext/unpacker.h
+++ include/ext/unpacker.h
@@ -55,12 +55,8 @@ public:
 	unpacker() {reset();}
 	size_t current_msg_length() const {return cur_msg_len;} //current msg's total length, -1 means not available
 
-	bool parse_msg(size_t bytes_transferred, boost::container::list<std::pair<const char*, size_t> >& msg_can)
+	bool parse_msg(boost::container::list<std::pair<const char*, size_t> >& msg_can)
 	{
-		//length + msg
-		remain_len += bytes_transferred;
-		assert(remain_len <= ST_ASIO_MSG_BUFFER_SIZE);
-
 		const char* pnext = raw_buff.begin();
 		bool unpack_ok = true;
 		while (unpack_ok) //considering sticky package problem, we need a loop
@@ -102,8 +98,12 @@ public:
 	virtual void dump_left_data() const {unpacker_helper::dump_left_data(raw_buff.data(), cur_msg_len, remain_len);}
 	virtual bool parse_msg(size_t bytes_transferred, container_type& msg_can)
 	{
+		//length + msg
+		remain_len += bytes_transferred;
+		assert(remain_len <= ST_ASIO_MSG_BUFFER_SIZE);
+
 		boost::container::list<std::pair<const char*, size_t> > msg_pos_can;
-		bool unpack_ok = parse_msg(bytes_transferred, msg_pos_can);
+		bool unpack_ok = parse_msg(msg_pos_can);
 		for (BOOST_AUTO(iter, msg_pos_can.begin()); iter != msg_pos_can.end(); ++iter)
 			if (iter->second > ST_ASIO_HEAD_LEN) //ignore heartbeat
 			{
@@ -143,7 +143,7 @@ public:
 				return 0;
 		}
 
-		return data_len >= cur_msg_len ? 0 : boost::asio::detail::default_max_transfer_size;
+		return data_len >= cur_msg_len ? 0 : ST_ASIO_MSG_BUFFER_SIZE;
 		//read as many as possible except that we have already got an entire msg
 	}
 
@@ -320,12 +320,12 @@ public:
 		if (0 == step) //want the head
 		{
 			assert(raw_buff.empty());
-			return boost::asio::detail::default_max_transfer_size;
+			return ST_ASIO_HEAD_LEN;
 		}
 		else if (1 == step) //want the body
 		{
 			assert(!raw_buff.empty());
-			return boost::asio::detail::default_max_transfer_size;
+			return raw_buff.size();
 		}
 		else
 			assert(false);
@@ -382,7 +382,7 @@ public:
 	//a return value of 0 indicates that the read operation is complete. a non-zero value indicates the maximum number
 	//of bytes to be read on the next call to the stream's async_read_some function. ---boost::asio::async_read
 	virtual size_t completion_condition(const boost::system::error_code& ec, size_t bytes_transferred)
-		{return ec || bytes_transferred == raw_buff.size() ? 0 : boost::asio::detail::default_max_transfer_size;}
+		{return ec || bytes_transferred == raw_buff.size() ? 0 : _fixed_length;}
 
 	//this is just to satisfy the compiler, it's not a real scatter-gather buffer,
 	//if you introduce a ring buffer, then you will have the chance to provide a real scatter-gather buffer.
@@ -437,7 +437,7 @@ public:
 				return 0; //invalid msg, stop reading
 		}
 
-		return boost::asio::detail::default_max_transfer_size; //read as many as possible
+		return ST_ASIO_MSG_BUFFER_SIZE; //read as many as possible
 	}
 
 	//like strstr, except support \0 in the middle of mem and sub_mem
@@ -541,7 +541,7 @@ public:
 		return true;
 	}
 
-	virtual size_t completion_condition(const boost::system::error_code& ec, size_t bytes_transferred) {return ec || bytes_transferred > 0 ? 0 : boost::asio::detail::default_max_transfer_size;}
+	virtual size_t completion_condition(const boost::system::error_code& ec, size_t bytes_transferred) {return ec || bytes_transferred > 0 ? 0 : ST_ASIO_MSG_BUFFER_SIZE;}
 
 	//this is just to satisfy the compiler, it's not a real scatter-gather buffer,
 	//if you introduce a ring buffer, then you will have the chance to provide a real scatter-gather buffer.
