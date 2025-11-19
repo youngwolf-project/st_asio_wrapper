2d6d6846ab90c4fd0b59ffb28da239ce4053e336
youngwolf <mail2tao@163.com>
2025-11-19 21:21:03 +0800

    Fix compilation error under MSYS2. Unix Socket now be compiled in MingW and Cygwin.

--- config.mk
+++ config.mk
@@ -3,7 +3,12 @@
 #boost_include_dir = -I/usr/local/include/
 #boost_lib_dir = -L/usr/local/lib/
 
-cflag = -Wall -fexceptions -std=c++98
+cflag = -Wall -fexceptions -std=c++98 #if your boost version is too hight, you may need to specify c++11
+
+ifndef WIN_VER
+	WIN_VER = -D_WIN32_WINNT=0x0501 #we may need to increase this version for example, to 0x0A00 (win 10)
+endif
+
 ifeq (${MAKECMDGOALS}, debug)
 	cflag += -g -DDEBUG
 	dir = debug
@@ -13,7 +18,8 @@ else
 	dir = release
 endif
 cflag += -DBOOST_ASIO_NO_DEPRECATED
-common_libs = -lboost_system -lboost_thread -lboost_chrono
+common_libs = -lboost_system -lboost_thread -lboost_chrono #in some ENV, the file name of boost libraries may end with -mt
+#if your boost version is too high, boost_system may not exists.
 
 target_machine = ${shell ${CXX} -dumpmachine}
 ifneq (, ${findstring solaris, ${target_machine}})
@@ -26,6 +32,24 @@ else
 		common_libs += -lboost_atomic
 		#here maybe still have other machines need to be separated out
 	endif
+
+	ifneq (, ${findstring cygwin, ${target_machine}})
+		cflag += -D__USE_W32_SOCKETS ${WIN_VER}
+		lflag += -lws2_32 -lwsock32
+	endif
+endif
+
+ifneq (, ${findstring mingw, ${target_machine}})
+	SHELL = cmd
+	cflag += -D__USE_MINGW_ANSI_STDIO=1 ${WIN_VER}
+	lflag += -lws2_32 -lwsock32
+	ignore = 1>nul
+	make_dir = md ${dir} 2>nul
+	del_dirs = -rd /S /Q debug release 2>nul
+else
+	ignore = 1>/dev/null
+	make_dir = mkdir -p ${dir}
+	del_dirs = -rm -rf debug release
 endif
 
 cflag += ${ext_cflag} ${boost_include_dir}
@@ -35,17 +59,16 @@ target = ${dir}/${module}
 sources = ${shell ls *.cpp}
 objects = ${patsubst %.cpp,${dir}/%.o,${sources}}
 deps = ${patsubst %.o,%.d,${objects}}
-${shell mkdir -p ${dir}}
+${shell ${make_dir}}
 
 release debug : ${target}
 -include ${deps}
 ${target} : ${objects}
 	${CXX} -o $@ $^ ${lflag}
 ${objects} : ${dir}/%.o : %.cpp
-	${CXX} ${cflag} -E -MMD -w -MT '$@' -MF ${subst .cpp,.d,${dir}/$<} $< 1>/dev/null
+	${CXX} ${cflag} -E -MMD -w -MT '$@' -MF ${subst .cpp,.d,${dir}/$<} $< ${ignore}
 	${CXX} ${cflag} -c $< -o $@
 
 .PHONY : clean
 clean:
-	-rm -rf debug release
-
+	${del_dirs}
--- makefile
+++ makefile
@@ -8,8 +8,6 @@ else
 	endif
 endif
 
-target_machine = ${shell ${CXX} -dumpmachine}
-
 release debug clean :
 	cd echo_server && ${ST_MAKE}
 	cd echo_client && ${ST_MAKE}
@@ -23,10 +21,5 @@ release debug clean :
 	cd socket_management && ${ST_MAKE}
 	cd udp_test && ${ST_MAKE}
 	cd ssl_test && ${ST_MAKE}
-ifeq (, ${findstring cygwin, ${target_machine}})
-ifeq (, ${findstring mingw, ${target_machine}})
 	cd unix_socket && ${ST_MAKE}
 	cd unix_udp_test && ${ST_MAKE}
-endif
-endif
-
