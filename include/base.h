7ee0d148cabf83f36af0752d0c258e6a23cbd35a
youngwolf <li.10.yang@nokia.com>
2025-11-26 16:05:18 +0800

    Format unification.

--- include/base.h
+++ include/base.h
@@ -1023,7 +1023,7 @@ public:
 		to_time_str(time(NULL), os);
 		os << " -> ";
 
-#if defined _MSC_VER || defined __clang__
+#if defined(_MSC_VER) || defined(__clang__)
 		os.rdbuf()->sgetn(buff, buff_len);
 #endif
 		size_t len = (size_t) os.tellp();
